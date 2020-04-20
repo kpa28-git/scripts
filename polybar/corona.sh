ad0f2315bab238cc3b044498fa876f7b4046a8ca
kevindirect <15056154+kevindirect@users.noreply.github.com>
2020-04-20 15:18:01 -0700

    fix corona, aesthetics, internet poll script

--- polybar/corona.sh
+++ polybar/corona.sh
@@ -7,7 +7,7 @@ getreport() {
 }
 
 showreport() {
-	grep "$1" "$HOME/.local/share/coronareport" | sed "s/\s*//g; s/║//g; s/│/;/g; s/\x1b\[[0-9;]*m//g" | awk -F';' '{print $3 " (" $9")"}';
+	grep "$1" "$HOME/.local/share/coronareport" | sed "s/\s*//g; s/║//g; s/│/;/g; s/\x1b\[[0-9;]*m//g" | awk -F';' '{print $3 " (" $5")"}';
 }
 
 ping -q -c 1 9.9.9.9 > /dev/null || (echo '' && exit 1);
