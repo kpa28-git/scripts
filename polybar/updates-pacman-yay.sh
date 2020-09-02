dc8f4673c8dc941f3e40d94526377b66177ff67e
kevindirect <15056154+kevindirect@users.noreply.github.com>
2020-09-02 11:51:42 -0700

    polybar aur/pacman available updates script

---
+++ polybar/updates-pacman-yay.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Modified from: https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/updates-pacman-aurhelper
+
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+	updates_arch=0;
+fi;
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+	updates_aur=0;
+fi;
+
+printf " %s  %s" $updates_aur $updates_arch;
+
