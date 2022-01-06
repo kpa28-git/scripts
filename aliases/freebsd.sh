19c1eb193ef3e8da7d4d0728223491cc22d36c65
kevindirect <git@kevindirect.com>
2022-01-05 17:43:33 -0800

    update freebsd aliases

--- aliases/freebsd.sh
+++ aliases/freebsd.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 # free bsd aliases
 
-alias fp="pkg install" \
+alias fp="sudo pkg install" \
 	fpls="pkg info" \
 	fpq="pkg info" \
 	fpg="pkg info | grep" \
+	fppls="pkg info -l" \
+	fppmsg="pkg info -D" \
 	fps="pkg search" \
-	fpu="sudo pkg update && sudo pkg upgrade";
+	fpu="sudo pkg update && sudo pkg upgrade" \
+	fprm="sudo pkg delete" \
+	fpcl="sudo pkg clean -a";
 
+alias servls="service -e" \
+	servq="service -e" \
+	servg="service -e | grep" \
+	servs="service -l" \
+	servrestart="service -R";
+
+alias lsdefkernmod="ls /boot/kernel | grep -v kernel";
