f583c146dc1ed40e29673ff6cd24e9241881c5de
kevindirect <15056154+kevindirect@users.noreply.github.com>
2023-08-19 08:47:31 -0700

    optimus-manager -> envycontrol

--- aliases/arch.sh
+++ aliases/arch.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # arch linux aliases
 
-# ap: arch packages (pacman)
-alias ap="sudo pacman -Syu" \
-	apsp="sudo pacman -Syu && poweroff" \
-	apsr="sudo pacman -Syu && reboot" \
-	aprmlck="sudo rm '/var/lib/pacman/db.lck'" \
-	apls="pacman -Qett --color=always" \
-	apwhen="expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort" \
-	apmirru="sudo reflector --country 'United States' --latest 200 --age 24 --sort rate --save /etc/pacman.d/mirrorlist && rm -f /etc/pacman.d/mirrorlist.pacnew";
+# pac: arch packages (pacman)
+alias pac="sudo pacman -Syu" \
+	pacsp="sudo pacman -Syu && poweroff" \
+	pacsr="sudo pacman -Syu && reboot" \
+	pacrmlck="sudo rm '/var/lib/pacman/db.lck'" \
+	pacls="pacman -Qett --color=always" \
+	pacwhen="expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort" \
+	pacmirru="sudo reflector --country 'United States' --latest 200 --age 24 --sort rate --save /etc/pacman.d/mirrorlist && rm -f /etc/pacman.d/mirrorlist.pacnew";
 
 # aur: arch user repo packages (paru)
 alias aurg="paru -Qs | grep -B1" \
